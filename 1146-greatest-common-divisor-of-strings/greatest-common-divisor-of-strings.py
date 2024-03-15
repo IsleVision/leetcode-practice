cc5308a4d97e4decbddb980befd2d0c858908ba5
IsleVision <32056680+IsleVision@users.noreply.github.com>
2024-03-15 19:28:28 +0800

    Time: 54 ms (6.49%) | Memory: 11.7 MB (79.69%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isDivisor(self, str_ini, sub_str):
+        return str_ini.replace(sub_str,'')==''
+        
+    def gcdOfStrings(self, str1, str2):
+        i,j = 0,0
+        length1 = len(str1)
+        length2 = len(str2)
+        gcd = ''
+        gcd_tmp = ''
+
+        while i < length1 and j < length2:
+            if str1[i]==str2[j]:
+                gcd_tmp += str1[i]
+            else:
+                break
+
+            if self.isDivisor(str1,gcd_tmp) and self.isDivisor(str2,gcd_tmp):
+                gcd = gcd_tmp   
+
+            i+=1
+            j+=1
+
+        return gcd    
+ 
+
+
+
+
+
+
+
