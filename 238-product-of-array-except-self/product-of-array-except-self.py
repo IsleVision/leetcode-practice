ca78400c57efaa9c4bea2c5c54ce605dcfa5c313
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-17 21:48:43 +0800

    Time: 163 ms (70.55%) | Memory: 21.7 MB (25.44%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -6,12 +6,10 @@ class Solution(object):
         res = [1]*length
         tmp = 1
         for i in range(1,length,1):
-            res1[i] = tmp*nums[i-1]
-            tmp = res1[i]
+            res1[i] = res1[i-1]*nums[i-1]
         tmp = 1    
         for i in range(length-2,-1,-1):
-            res2[i] = tmp*nums[i+1]
-            tmp = res2[i] 
+            res2[i] = res2[i+1]*nums[i+1]
             res[i]=res1[i]*res2[i]
         res[length-1]=res1[length-1]    
         return res
