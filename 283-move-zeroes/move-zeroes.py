a4bc03e32ff8ec803467b4e90e5ebe284117d8df
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-18 21:43:58 +0800

    Time: 117 ms (62.49%) | Memory: 12.9 MB (37.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,12 +1,23 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        i,j = 0,0
-        length = len(nums)
-        for i in range(length):
-            if nums[i] ==0:
-                for j in range(i+1,length,1):
-                    if nums[j]!=0:
-                        nums[i]=nums[j]
-                        nums[j]=0
-                        break
-        return nums   
+        #solution one
+        # i,j = 0,0
+        # length = len(nums)
+        # for i in range(length):
+        #     if nums[i] ==0:
+        #         for j in range(i+1,length,1):
+        #             if nums[j]!=0:
+        #                 nums[i]=nums[j]
+        #                 nums[j]=0
+        #                 break
+        # return nums
+
+        #solution2
+        i,j=0,0
+        for j in range(len(nums)):
+            if nums[j]!=0 and nums[i]==0:
+                nums[i]=nums[j]
+                nums[j]=0
+            if nums[i]!=0:
+                i+=1
+            
