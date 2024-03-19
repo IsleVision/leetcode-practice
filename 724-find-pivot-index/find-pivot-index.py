4355171eef4e8611523cc3b26050d65d644c7d6e
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-19 21:21:46 +0800

    Time: 111 ms (50.72%) | Memory: 12.5 MB (99.74%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,6 +1,18 @@
 class Solution(object):
     def pivotIndex(self, nums):
+        #solution1
+        # for i in range(len(nums)):
+        #     if sum(nums[:i])==sum(nums[i+1:]):
+        #         return i
+        # return -1
+
+        #solution2
+        sum_r = sum(nums)
+        sum_l = 0
         for i in range(len(nums)):
-            if sum(nums[:i])==sum(nums[i+1:]):
+            sum_r -= nums[i]
+            sum_l += nums[i-1] if i>0 else 0
+            if sum_r==sum_l:
                 return i
         return -1
+
