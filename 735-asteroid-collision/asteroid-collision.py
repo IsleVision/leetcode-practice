d459e486c6721e295d3964ccd065ab254cf1f8af
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-21 19:26:19 +0800

    Time: 56 ms (96.53%) | Memory: 12.8 MB (44.88%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,25 +1,35 @@
 class Solution(object):
-    def resolveNew(self,asteroids,new):
-        if asteroids==[]:
-            asteroids += [new]
-            return
-        if (asteroids[-1]<0 and new<0) or (asteroids[-1]>0 and new>0) or (asteroids[-1]<0 and new>0):
-            asteroids += [new]
-        elif abs(asteroids[-1])<abs(new):
-            asteroids.pop()
-            self.resolveNew(asteroids,new)
-        elif abs(asteroids[-1])==abs(new):
-            asteroids.pop()
+    # def resolveNew(self,asteroids,new):
+    #     if asteroids==[]:
+    #         asteroids += [new]
+    #         return
+    #     if (asteroids[-1]<0 and new<0) or (asteroids[-1]>0 and new>0) or (asteroids[-1]<0 and new>0):
+    #         asteroids += [new]
+    #     elif abs(asteroids[-1])<abs(new):
+    #         asteroids.pop()
+    #         self.resolveNew(asteroids,new)
+    #     elif abs(asteroids[-1])==abs(new):
+    #         asteroids.pop()
+
+    # def asteroidCollision(self, asteroids):
+    #     res = asteroids[0:1]
+    #     for i in range(1,len(asteroids),1):
+    #         self.resolveNew(res,asteroids[i])
+    #     return res
 
     def asteroidCollision(self, asteroids):
-        res = asteroids[0:1]
-        for i in range(1,len(asteroids),1):
-            self.resolveNew(res,asteroids[i])
+        res = []
+        for ast in asteroids:
+            while True:
+                if res==[] or res[-1]<0 or ast>0:
+                    res += [ast]
+                    break
+                elif res[-1]>-ast:
+                    break
+                elif res[-1]==-ast:
+                    res.pop()
+                    break
+                else:
+                    res.pop()
+
         return res
-
-                
-
-
-
-
-        
