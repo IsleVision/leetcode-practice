5d02f3755a88499cc08b5bba0245a04ead4d5084
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-24 00:33:54 +0800

    Time: 38 ms (45.86%) | Memory: 16.5 MB (94.09%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -10,10 +10,10 @@ class Solution:
         leaves = []
         while nd_arr:
             nd_tmp,dep = nd_arr.pop()
-            if nd_tmp.left:
-                nd_arr +=[[nd_tmp.left,dep+1]]
             if nd_tmp.right:
                 nd_arr +=[[nd_tmp.right,dep+1]]
+            if nd_tmp.left:
+                nd_arr +=[[nd_tmp.left,dep+1]]
             if not nd_tmp.left and not nd_tmp.right:
                 leaves += [nd_tmp.val]
         return leaves
