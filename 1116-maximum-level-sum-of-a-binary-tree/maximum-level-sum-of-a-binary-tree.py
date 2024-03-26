eff747cab4c5d426cc87c006754dafab8d8b80ef
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-26 19:35:47 +0800

    Time: 264 ms (5.03%) | Memory: 20.1 MB (32.11%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -8,23 +8,25 @@ class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         nd_arr = [[root,0]]
         res = []
+        max_v = root.val
+        l=1
         while nd_arr:
             [nd,dep]=nd_arr.pop(0)
             if not nd:
                 continue
             if len(res)-1<dep:
+                if res and res[-1]>max_v:
+                    max_v = res[-1]
+                    l=dep
                 res +=[nd.val]
             else:
                 res[dep] +=nd.val
             nd_arr+=[[nd.left,dep+1]]
             nd_arr+=[[nd.right,dep+1]]
-        max_v=res[0]
-        l = 1
-        for i in range(len(res)):
-            if res[i]>max_v:
-                max_v=res[i]
-                l=i+1
-        return l
+        if res[-1]>max_v:
+            return len(res)
+        else:
+            return l
 
 
 
