e7d6fb246b90f717e7855faaf952f12b2a128e19
Tao <32056680+IsleVision@users.noreply.github.com>
2024-03-27 17:00:35 +0800

    Time: 188 ms (45.28%) | Memory: 17.3 MB (84.01%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def findCon( isConnected: List[List[int]], cons:Set[int], ci:int):
+            for i in range(len(isConnected[ci])):
+                if isConnected[ci][i]==1 and i not in cons:
+                    cons.add(i)
+                    cons |= findCon(isConnected,cons,i)
+            return cons
+
+        prov = {}
+        cities = set(range(len(isConnected)))
+        group_num =0
+        while cities:
+            city = cities.pop()
+            cons = findCon(isConnected,{city},city)
+            print(cons)
+            if cons:
+                group_num +=1
+                cities -=cons
+        return group_num
+
+
