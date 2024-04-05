c4e74b2aec8373d051a3d8fb96d438997c6c4a50
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-06 01:25:17 +0800

    Time: 43 ms (78.86%) | Memory: 16.6 MB (87.16%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,34 +1,21 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # length = len(nums)
-        # idx = 0
-        # while length>2:
-        #     mid = length//2
-        #     if nums[mid]>=nums[mid+1] and nums[mid]>=nums[mid-1]:
-        #         return idx + mid
-        #     elif nums[mid]<nums[mid+1]: 
-        #         nums = nums[mid+1:]
-        #         idx += mid
-        #     else:
-        #         nums = nums[0:mid]
-        #         idx -=mid
-        #     length = len(nums)
-        # if length==1:
-        #     return
-            
-        n = len(nums)
-        low, high = 0, n-1
-        while low <= high:
-            mid = ((high-low)//2) + low
-            # Peak element is present in right side
-            if mid < n-1 and nums[mid] < nums[mid+1]:
-                low = mid+1
-            # Peak element is present in left side
-            elif mid > 0 and nums[mid] < nums[mid-1]:
-                high = mid-1
-            else:
+        length = len(nums)
+        l,r = 0, length-1
+        while r-l>1:
+            mid = (l+r)//2
+            if nums[mid]>=nums[mid-1] and nums[mid]>=nums[mid+1]:
                 return mid
-            
+            elif nums[mid]<nums[mid-1]:
+                r=mid
+            else:
+                l=mid
+        if r-l==0:
+            return 0
+        else:
+            return l if nums[l]>=nums[r] else r
+
+
 
                
         
