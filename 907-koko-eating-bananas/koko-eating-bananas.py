3b8dfde2da074ffc801e9ee622120c41872f3ea1
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-06 20:11:19 +0800

    Time: 266 ms (62.16%) | Memory: 18.3 MB (14.61%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -9,18 +9,17 @@ class Solution:
         # min_h = -(piles[-1]//-(q+1))
         # min_l = -(piles[length-r-1]//-q)
         # return max(min_h,min_l)
-        def resolveTime(piles:List[int],speed:int)->int:
-            times = []
-            for pile in piles:
-                times += [-(pile//-speed)]
-            return sum(times)
-        piles.sort()
-        l,r=1,max(piles)
-        while l!=r:
-            speed = (l+r)//2
-            if resolveTime(piles,speed)>h:
-                l=speed+1
+        l, r = 1, max(piles)
+        
+        def isSufficientSpeed(cnt):
+            return sum(ceil(i/cnt) for i in piles) <= h
+
+        while l < r:
+            m = (l + r)//2
+            if isSufficientSpeed(m):
+                r = m
             else:
-                r=speed
+                l = m + 1
+                
         return l
 
