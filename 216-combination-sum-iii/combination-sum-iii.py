811e56d06aaa493f533811b7d50399a07e75c7a2
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-10 23:31:35 +0800

    Time: 38 ms (44.07%) | Memory: 16.5 MB (27.73%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,39 +1,24 @@
 class Solution:
-    # def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-    #     ans = []
-    #     def search(start:int, k:int, n:int, res: List[int]):
-    #         nonlocal ans
-    #         if n<1:
-    #             return
-    #         if k==1:
-    #             if n<start:
-    #                 return
-    #             else:
-    #                 print(res)
-    #                 res+=[n]
-    #                 ans +=[res]
-    #                 return
-    #         for i in range(res[-1]+1,10):
-    #             res = res.copy()
-    #             res +=[i]
-    #             search(i,k-1,n-i,res)
-                
-    #     for i in range(1,10):
-    #         search(i,k-1,n-i,[i])
-    #     return ans
-            
-            
-            
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def backtrack(start, target, k, res):
-            if k == 0 and target == 0:
-                return [[]]
-            if k == 0 or target < 0:
-                return []
-            ans = []
-            for i in range(start, 10):
-                for combo in backtrack(i+1, target-i, k-1, res):
-                    ans.append([i] + combo)
-            return ans
-        
-        return backtrack(1, n, k, [])
+        ans = []
+        def search(start:int, k:int, n:int, res: List[int]):
+            nonlocal ans
+            if n<1:
+                return
+            if k==1:
+                if n<=start or n>9:
+                    return
+                else:
+                    print(res)
+                    ans +=[res+[n]]
+                    return
+            for i in range(res[-1]+1,10):
+                search(i,k-1,n-i,res+[i])
+                
+        for i in range(1,10):
+            search(i,k-1,n-i,[i])
+        return ans
+            
+            
+            
+
