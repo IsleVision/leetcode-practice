4f9579b8388a4bb0d4632f2f93ee1fd163ee4468
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-11 23:11:01 +0800

    Time: 1098 ms (34.93%) | Memory: 62.3 MB (87.63%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -5,8 +5,8 @@ class Solution:
         comm_e=points[0][1]
         cnt = 1
         for pt in points:
-            if pt[0]>comm_s and pt[0]<=comm_e:
-                comm_s=pt[0]
+            # if pt[0]>comm_s and pt[0]<=comm_e:
+                # comm_s=pt[0]
             if pt[1]<comm_e:
                 comm_e=pt[1]
             if pt[0]>comm_e:
