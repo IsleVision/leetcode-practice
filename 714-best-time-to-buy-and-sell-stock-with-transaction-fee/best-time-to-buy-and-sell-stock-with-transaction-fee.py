3898b93463e7f54cf241e1f0e1bcbaf4606285ac
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-19 23:30:27 +0800

    Time: 442 ms (97.49%) | Memory: 23.6 MB (86.49%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        if len(prices)==1:
+            return 0
+        buy=prices[0]
+        sell=0
+        res =0
+        for pr in prices:
+            if pr+fee<=sell:
+                if sell>buy+fee:
+                    res+=sell-buy-fee
+                buy=pr
+                sell=0
+            elif pr<=buy:
+                buy=pr
+            elif pr>sell:
+                sell=pr
+        if sell!=0 and sell>buy+fee:
+            res+=sell-buy-fee
+        return res
+        
