61cbf1f4f314cfd668251f774bdabc4978e9fd22
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-20 23:25:00 +0800

    Time: 29 ms (88.42%) | Memory: 16.6 MB (57.22%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -3,9 +3,12 @@ class Solution:
         cnt=0
         a_b_c= a or b or c
         while a_b_c>0:
-            a_bit = a%2
-            b_bit = b%2
-            c_bit = c%2
+            # a_bit = a%2
+            # b_bit = b%2
+            # c_bit = c%2
+            a_bit = a&1
+            b_bit = b&1
+            c_bit = c&1
             if c_bit==1 and a_bit==0 and b_bit==0:
                 cnt+=1
             elif c_bit==0:
@@ -14,8 +17,11 @@ class Solution:
                 #     cnt+=1
                 # if b_bit==1:
                 #     cnt+=1
-            a=a//2
-            b=b//2
-            c=c//2
+            # a=a//2
+            # b=b//2
+            # c=c//2
+            a=a>>1
+            b=b>>1
+            c=c>>1
             a_b_c=a or b or c
         return cnt
