c67d63e65b2defda1faf9f7a4e791f6d66d1f802
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-24 16:19:35 +0800

    Time: 305 ms (11.39%) | Memory: 21.2 MB (86.28%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,16 +1,28 @@
 class StockSpanner:
 
-    def __init__(self):
-        self.prices=[]
+    # def __init__(self):
+    #     self.prices=[]
         
 
-    def next(self, price: int) -> int:
-        self.prices+=[price]
-        length = len(self.prices)
-        for i in range(length-1,-1,-1):
-            if self.prices[i]>price:
-                return length-1-i
-        return length
+    # def next(self, price: int) -> int:
+    #     self.prices+=[price]
+    #     length = len(self.prices)
+    #     for i in range(length-1,-1,-1):
+    #         if self.prices[i]>price:
+    #             return length-1-i
+    #     return length
+
+    def __init__(self):
+        self.mono_stack=[]
+
+    def next(self,price:int) -> int:
+        cur_span = 1
+        while self.mono_stack and self.mono_stack[-1][0]<=price:
+            pre_price,pre_span=self.mono_stack.pop()
+            cur_span +=pre_span
+        self.mono_stack +=[[price,cur_span]]
+        return cur_span
+
         
 
 # Your StockSpanner object will be instantiated and called as such:
