c983b2d3bd1c5dd3d8d3730b1b88362d13fc62f9
Tao <32056680+IsleVision@users.noreply.github.com>
2024-04-24 22:16:53 +0800

    Time: 209 ms (34.26%) | Memory: 19.7 MB (80.86%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -0,0 +1,20 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        length = len(searchWord)
+        res=[[] for _ in range(length)]
+        print(res)
+        for i in range(length):
+            wd = searchWord[0:i+1]
+            cnt=0
+            for pd in products:
+                if pd.find(wd)==0:
+                    res[i]+=[pd]
+                    cnt+=1
+            if i==0:
+                res[i].sort()
+                products=res[i]
+            if cnt>3:
+                res[i]=res[i][:3]
+        return res
+
+        
