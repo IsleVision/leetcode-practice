0af6a805960c26d183a91a3987448559f1880ec7
Tao <32056680+IsleVision@users.noreply.github.com>
2024-05-08 18:31:17 +0800

    Time: 122 ms (7.54%) | Memory: 18.5 MB (32.12%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+        pathDic = {}
+        for p in prerequisites:
+            if p[0] not in pathDic:
+                pathDic[p[0]]=[p[1]]
+            else:
+                pathDic[p[0]]+=[p[1]]
+        notVis = pathDic.keys()-{prerequisites[0][0]}
+        arr = [[prerequisites[0][0]]]
+        while arr or notVis:
+            if not arr:
+                arr = [[notVis.pop()]]
+            nd = arr[0][-1]
+            if nd not in pathDic or not pathDic[nd]:
+                arr.pop(0)
+            else:
+                ary = arr[0]+[]
+                for i in range(len(pathDic[nd])):
+                    nd_nx = pathDic[nd][i]
+                    # print(arr,ary)
+                    if nd_nx in ary:
+                        return False
+                    elif i==0:
+                        arr[0]+=[nd_nx]
+                    else:
+                        arr += [ary+[nd_nx]]
+                    notVis -= {nd_nx}
+                del pathDic[nd]
+        return True
