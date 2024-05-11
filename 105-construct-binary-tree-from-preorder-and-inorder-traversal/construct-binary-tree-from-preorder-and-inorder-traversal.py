5e818475cbd06bbb9fbe8da8eee708de98f5625c
Tao <32056680+IsleVision@users.noreply.github.com>
2024-05-11 17:34:58 +0800

    Time: 111 ms (63.74%) | Memory: 88.3 MB (11.11%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,19 +6,40 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        #solution 1
+        # if not preorder:
+        #     return None
+        # nd = TreeNode(preorder[0],None,None)
+        # if not preorder[1:]:
+        #     return nd
+        # idx = inorder.index(preorder[0])
+        # idx_n = inorder.index(preorder[1])
+        # if idx==idx_n-1:
+        #     nd.right = self.buildTree(preorder[idx+1:],inorder[idx+1:])
+        # else:
+        #     nd.left =self.buildTree(preorder[1:idx+1],inorder[0:idx])
+        #     nd.right=self.buildTree(preorder[idx+1:],inorder[idx+1:])
+        # return nd
+
+        #solution 2
         if not preorder:
             return None
         nd = TreeNode(preorder[0],None,None)
-        if not preorder[1:]:
+        length=len(preorder)
+        if length==1:
             return nd
         idx = inorder.index(preorder[0])
-        idx_n = inorder.index(preorder[1])
-        if idx==idx_n-1:
-            nd.right = self.buildTree(preorder[idx+1:],inorder[idx+1:])
+        if idx==0:
+            nd.right=self.buildTree(preorder[1:],inorder[1:])
+        elif idx==length-1:
+            nd.left=self.buildTree(preorder[1:],inorder[:-1])
         else:
-            nd.left =self.buildTree(preorder[1:idx+1],inorder[0:idx])
+            nd.left= self.buildTree(preorder[1:idx+1],inorder[:idx])
             nd.right=self.buildTree(preorder[idx+1:],inorder[idx+1:])
         return nd
+        
+
+
         
 
         
