f955c57a1239d1cff2b8e6f3cf20aa1f34ced916
Tao <32056680+IsleVision@users.noreply.github.com>
2025-01-18 11:34:31 +0800

    Time: 15 ms (17.34%) | Memory: 19.5 MB (11.18%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         dic = {}
-        for i in range(len(nums)):
-            if nums[i] in dic:
-                dic[nums[i]] +=1
+        # for i in range(len(nums)):
+        #     if nums[i] in dic:
+        #         dic[nums[i]] +=1
+        #     else:
+        #         dic[nums[i]] = 1
+        # for k in dic:
+        #     if dic[k] > len(nums)/2:
+        #         return k
+
+        for num in nums:
+            if num in dic:
+                dic[num] +=1
             else:
-                dic[nums[i]] = 1
+                dic[num] = 1
         for k in dic:
             if dic[k] > len(nums)/2:
                 return k
