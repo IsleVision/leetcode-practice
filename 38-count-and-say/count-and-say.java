1adedd07c00e1c8c8c083b8f074b47fd625af7a4
Tao <32056680+IsleVision@users.noreply.github.com>
2025-04-19 00:08:59 +0800

    Time: 24 ms (5.80%) | Memory: 45.5 MB (5.96%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -6,17 +6,16 @@ class Solution {
 
         String temp = countAndSay(n-1);
         int length = temp.length();
-        StringBuilder result = new StringBuilder("1"+temp.charAt(0));
+        String result = "1"+temp.charAt(0);
         for(int i=1; i<length; i++){
             if(result.charAt(result.length()-1)!=temp.charAt(i)){
-                result.append("1"+temp.charAt(i));
+                result +="1"+temp.charAt(i);
             }
             else{
                 int cnt = result.charAt(result.length()-2)-'0';
-                result.setCharAt(result.length()-2,((cnt+1)+"").charAt(0));
+                result = result.substring(0,result.length()-2)+(cnt+1)+result.substring(result.length()-1);
             }
         }
-        System.out.println(n+" "+temp+" "+result);
-        return result.toString();
+        return result;
     }
 }
