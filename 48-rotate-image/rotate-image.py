f0bf86cf7985538dec59a2fb17df539070bfd125
Tao <32056680+IsleVision@users.noreply.github.com>
2025-04-28 20:43:50 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (67.15%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -7,14 +7,8 @@ class Solution:
         for i in range(len(matrix)//2):
             for j in range(i,length-i-1,1):
                 temp=matrix[i][j]
-
                 matrix[i][j] = matrix[length-j-1][i]
-
                 matrix[length-j-1][i] = matrix[length-i-1][length-j-1]
-
                 matrix[length-i-1][length-j-1] = matrix[j][length-i-1]
-
                 matrix[j][length-i-1] = temp
-
-                print(i,j,length-j-1,length-i-1, i+j, temp,matrix)
         
