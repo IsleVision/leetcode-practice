5937ce4b49b6b6663f69182760a2cb8ed4ba36bd
Tao <32056680+IsleVision@users.noreply.github.com>
2025-05-11 22:07:26 +0800

    Time: 17 ms (13.87%) | Memory: 45.4 MB (38.87%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        List<List<String>> al = new LinkedList<>();
+        int alIdx = 0;
+        String dir = "acc";
+        for(int i=0; i<s.length(); i++){
+            String sub = s.substring(i,i+1);
+            // System.out.println(alIdx);
+            // System.out.println(sub);
+            if(alIdx>=al.size() && alIdx<=numRows){
+                al.add(new LinkedList<String>(Arrays.asList(sub)));
+            }
+            else{
+                List l = al.get(alIdx);
+                l.add(sub);
+                // System.out.println(l);
+                // System.out.println(alIdx);
+            }
+
+            if(numRows==1){
+                alIdx=0;
+            }
+            else if(alIdx==numRows-1 && dir.equals("acc")){
+                alIdx--;
+                dir = "dec";
+            }
+            else if(alIdx==0 && dir.equals("dec")){
+                alIdx++;
+                dir = "acc";
+            }
+            else if(dir.equals("acc")){
+                alIdx++;
+            }
+            else if(dir.equals("dec")){
+                alIdx--;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<al.size(); i++){
+            List l = al.get(i);
+            while(l.size()>0){
+                sb.append(l.remove(0));
+            }
+        }
+        return sb.toString();
+    }
+}
